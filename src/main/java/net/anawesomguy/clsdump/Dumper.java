feb36357d6d698155b5b586d596007c5c0d7e044
Steve D <thefamilydavis@gmail.com>
2024-12-14 20:04:36 +1000

    Update Dumper.java

--- src/main/java/net/anawesomguy/clsdump/Dumper.java
+++ src/main/java/net/anawesomguy/clsdump/Dumper.java
@@ -1,5 +1,9 @@
 package net.anawesomguy.clsdump;
 
+import org.objectweb.asm.ClassVisitor;
+import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Opcodes;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.lang.instrument.ClassFileTransformer;
@@ -7,84 +11,49 @@ import java.lang.instrument.Instrumentation;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
 import java.security.ProtectionDomain;
-import java.util.Comparator;
-import java.util.stream.Stream;
 
 public final class Dumper implements ClassFileTransformer {
-    private static final Path DIR = Paths.get("_classdump");
-
-    static {
-        if (Files.exists(DIR))
-            try {
-                try (Stream<Path> walk = Files.walk(DIR)) {
-                    walk.sorted(Comparator.reverseOrder()).forEach(Dumper::rm);
-                }
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-    }
-
-    private static void rm(Path path) {
-        try {
-            Files.delete(path);
-        } catch (IOException e) {
-            throw new RuntimeException(path.toString(), e);
-        }
-    }
-
-    private final boolean debug;
-
-    public Dumper(boolean debug) {
-        this.debug = debug;
-    }
+    private static final String TARGET_METHOD_NAME = "yourPrivateMethod"; // Replace with your private method name
+    private static final String TARGET_METHOD_DESC = "()V"; // Replace with your method descriptor
+    private static final String STATIC_RESPONSE = "Static Response"; // The response you want to return
 
     public static void premain(String args, Instrumentation inst) {
-        System.out.println("Dump Classes to " + DIR.toAbsolutePath());
-        boolean debug = "debug".equalsIgnoreCase(args);
-        inst.addTransformer(new Dumper(debug));
-
-        //already loaded classes
-        for (Class<?> cls : inst.getAllLoadedClasses()) {
-            if (cls.isArray())
-                continue;
-            String name = cls.getName().replace('.', '/');
-            try {
-                String clsName = name + ".class";
-                InputStream in = cls.getResourceAsStream(clsName.substring(name.lastIndexOf('/') + 1));
-                if (in != null) {
-                    try {
-                        Path path = DIR.resolve(clsName);
-                        Files.createDirectories(path.getParent());
-                        Files.copy(in, path, StandardCopyOption.REPLACE_EXISTING);
-                    } finally {
-                        in.close();
-                    }
-                    if (debug)
-                        System.out.println("Dumped class " + name);
-                } else if (debug)
-                    System.out.println("Couldn't get bytes for " + name);
-            } catch (Exception e) {
-                System.err.println("Unable to dump class " + name);
-                //noinspection CallToPrintStackTrace
-                e.printStackTrace();
-            }
-        }
+        System.out.println("Intercepting method calls");
+        inst.addTransformer(new Dumper());
     }
 
+    @Override
     public byte[] transform(ClassLoader classLoader, String name, Class<?> clazz, ProtectionDomain protectionDomain, byte[] buf) {
-        try {
-            Path path = DIR.resolve(name + ".class");
-            Files.createDirectories(path.getParent());
-            Files.write(path, buf);
-            if (debug)
-                System.out.println("Dumped class " + name);
-        } catch (Exception e) {
-            System.err.println("Unable to dump class " + name);
-            //noinspection CallToPrintStackTrace
-            e.printStackTrace();
+        if (name.equals("your/package/ClassName")) { // Replace with the full class name containing the target method
+            return interceptMethod(buf);
         }
-        return null;
+        return null; // No transformation for other classes
+    }
+
+    private byte[] interceptMethod(byte[] classfileBuffer) {
+        ClassReader classReader = new ClassReader(classfileBuffer);
+        ClassWriter classWriter = new ClassWriter(ClassWriter.COMPUTE_FRAMES);
+        ClassVisitor classVisitor = new ClassVisitor(Opcodes.ASM9, classWriter) {
+            @Override
+            public MethodVisitor visitMethod(int access, String name, String descriptor, String signature, String[] exceptions) {
+                MethodVisitor mv = super.visitMethod(access, name, descriptor, signature, exceptions);
+                if (name.equals(TARGET_METHOD_NAME) && descriptor.equals(TARGET_METHOD_DESC)) {
+                    return new MethodVisitor(Opcodes.ASM9, mv) {
+                        @Override
+                        public void visitCode() {
+                            super.visitCode();
+                            // Return a static response
+                            mv.visitLdcInsn(STATIC_RESPONSE);
+                            mv.visitInsn(Opcodes.POP); // Pop the response if not used
+                            mv.visitInsn(Opcodes.RETURN); // Return from the method
+                        }
+                    };
+                }
+                return mv; // No modification for other methods
+            }
+        };
+        classReader.accept(classVisitor, 0);
+        return classWriter.toByteArray();
     }
 }
